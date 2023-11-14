52039c72745742b50ed38788e4d85a293a9b00fe
Victor Glindås <glinkis@gmail.com>
2023-11-14 19:22:35 +0100

    update build system

---
+++ tests/builder.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from "bun:test";
+import { configureMockBuilder } from "../src/builder.js";
+import { faker } from "@faker-js/faker";
+
+describe("builder", () => {
+  const { createMockBuilder } = configureMockBuilder({
+    faker: faker,
+  });
+
+  it("should create a mock builder", () => {
+    const buildMock = createMockBuilder((f) => ({
+      foo: f.animal.cat(),
+      bar: f.animal.dog(),
+    }));
+
+    const mocked = buildMock();
+
+    expect(mocked).toEqual({ foo: mocked.foo, bar: mocked.bar });
+  });
+
+  it("should override the default mock", () => {
+    const buildMock = createMockBuilder((f) => ({
+      foo: f.animal.cat(),
+      bar: f.animal.dog(),
+    }));
+
+    const mocked = buildMock({ foo: "baz" });
+
+    expect(mocked).toEqual({ foo: "baz", bar: mocked.bar });
+  });
+});
