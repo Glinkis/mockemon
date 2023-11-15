5798d20b1c056fe5569d74d5cd3a710b5c5b8d6f
Victor Glindås <glinkis@gmail.com>
2023-11-15 20:09:45 +0100

    update tests

--- tests/builder.test.ts
+++ tests/builder.test.ts
@@ -9,43 +9,47 @@ describe("builder", () => {
   }
 
   const { createMockBuilder } = configureMockBuilder({
-    faker: faker,
+    faker: {
+      name: () => "Gustavo",
+      pet: () => "Dog",
+      numbers: () => [1, 2, 3],
+    },
   });
 
   it("allows mocking objects", () => {
     const buildMock = createMockBuilder<PetOwner>((f) => ({
-      name: f.person.fullName(),
-      pet: f.animal.dog(),
+      name: f.name(),
+      pet: f.pet(),
     }));
 
     const mocked = buildMock();
 
     expect(mocked).toEqual({
-      name: mocked.name,
-      pet: mocked.pet,
+      name: "Gustavo",
+      pet: "Dog",
     });
   });
 
   it("allows overriding object mocks", () => {
     const buildMock = createMockBuilder<PetOwner>((f) => ({
-      name: f.person.fullName(),
-      pet: f.animal.dog(),
+      name: f.name(),
+      pet: f.pet(),
     }));
 
     const mocked = buildMock({
-      name: "baz",
+      name: "Rafael",
     });
 
     expect(mocked).toEqual({
-      name: "baz",
-      pet: mocked.pet,
+      name: "Rafael",
+      pet: "Dog",
     });
   });
 
   it("allows overriding object mocks with a function", () => {
-    const buildMock = createMockBuilder<PetOwner>((f) => ({
-      name: f.person.fullName(),
-      pet: f.animal.dog(),
+    const buildMock = createMockBuilder<PetOwner>(() => ({
+      name: "Gustavo",
+      pet: "Dog",
     }));
 
     const mocked = buildMock(() => ({
@@ -53,20 +57,19 @@ describe("builder", () => {
     }));
 
     expect(mocked).toEqual({
-      name: mocked.name,
+      name: "Gustavo",
       pet: "Parrot",
     });
   });
 
   it("allows mocking primitive values", () => {
-    const buildMock = createMockBuilder((f) => f.animal.cat());
+    const buildMock = createMockBuilder((f) => f.name());
 
-    const mocked = buildMock();
-    expect(mocked).toEqual(mocked);
+    expect(buildMock()).toEqual("Gustavo");
   });
 
   it("allows overriding primitive value mocks", () => {
-    const buildMock = createMockBuilder<string | null | undefined>((f) => f.animal.cat());
+    const buildMock = createMockBuilder<string | null | undefined>((f) => f.name());
 
     expect(buildMock("Parrot")).toEqual("Parrot");
 
@@ -76,7 +79,7 @@ describe("builder", () => {
   });
 
   it("allows overriding primitive value mocks with a function", () => {
-    const buildMock = createMockBuilder<string | null | undefined>((f) => f.animal.cat());
+    const buildMock = createMockBuilder<string | null | undefined>((f) => f.name());
 
     expect(buildMock(() => "Parrot")).toEqual("Parrot");
 
@@ -86,7 +89,7 @@ describe("builder", () => {
   });
 
   it("allows mocking arrays", () => {
-    const buildMock = createMockBuilder((f) => f.helpers.arrayElements([1, 2, 3]));
+    const buildMock = createMockBuilder((f) => f.numbers());
 
     const mocked = buildMock();
 
@@ -94,27 +97,14 @@ describe("builder", () => {
   });
 
   it("allows overriding array mocks", () => {
-    const buildMock = createMockBuilder<number[]>((f) => f.helpers.arrayElements([1, 2, 3]));
+    const buildMock = createMockBuilder<number[]>((f) => f.numbers());
 
     expect(buildMock([4, 5, 6])).toEqual([4, 5, 6]);
   });
 
   it("allows overriding array mocks with a function", () => {
-    const buildMock = createMockBuilder<number[]>((f) => f.helpers.arrayElements([1, 2, 3]));
+    const buildMock = createMockBuilder<number[]>((f) => f.numbers());
 
     expect(buildMock(() => [4, 5, 6])).toEqual([4, 5, 6]);
-  });
-
-  it("allows mocking nested objects", () => {
-    const buildMock = createMockBuilder((f) => ({
-      name: f.person.fullName(),
-      pet: {
-        name: f.animal.dog(),
-      },
-    }));
-
-    const mocked = buildMock();
-
-    expect(mocked).toEqual(mocked);
   });
 });
