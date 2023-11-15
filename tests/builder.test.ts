3b4d943010c9e93097572d1716b86b421146bde0
Victor Glindås <glinkis@gmail.com>
2023-11-15 18:04:29 +0100

    update test

--- tests/builder.test.ts
+++ tests/builder.test.ts
@@ -3,80 +3,85 @@ import { configureMockBuilder } from "../src/builder.js";
 import { faker } from "@faker-js/faker";
 
 describe("builder", () => {
+  interface PetOwner {
+    name: string;
+    pet: string;
+  }
+
   const { createMockBuilder } = configureMockBuilder({
     faker: faker,
   });
 
   it("should create a mock builder", () => {
-    const buildMock = createMockBuilder((f) => ({
-      foo: f.animal.cat(),
-      bar: f.animal.dog(),
+    const buildMock = createMockBuilder<PetOwner>((f) => ({
+      name: f.person.fullName(),
+      pet: f.animal.dog(),
     }));
 
     const mocked = buildMock();
 
-    expect(mocked).toEqual({ foo: mocked.foo, bar: mocked.bar });
+    expect(mocked).toEqual({
+      name: mocked.name,
+      pet: mocked.pet,
+    });
   });
 
   it("should allow overriding the default mock", () => {
-    const buildMock = createMockBuilder((f) => ({
-      foo: f.animal.cat(),
-      bar: f.animal.dog(),
+    const buildMock = createMockBuilder<PetOwner>((f) => ({
+      name: f.person.fullName(),
+      pet: f.animal.dog(),
     }));
 
-    const mocked = buildMock({ foo: "baz" });
+    const mocked = buildMock({
+      name: "baz",
+    });
 
-    expect(mocked).toEqual({ foo: "baz", bar: mocked.bar });
+    expect(mocked).toEqual({
+      name: "baz",
+      pet: mocked.pet,
+    });
   });
 
   it("should allow overriding the default mock with a function", () => {
-    const buildMock = createMockBuilder((f) => ({
-      foo: f.animal.cat(),
-      bar: f.animal.dog(),
+    const buildMock = createMockBuilder<PetOwner>((f) => ({
+      name: f.person.fullName(),
+      pet: f.animal.dog(),
     }));
 
-    const mocked = buildMock((f) => ({ foo: "Parrot" as const }));
+    const mocked = buildMock(() => ({
+      pet: "Parrot",
+    }));
 
-    expect(mocked).toEqual({ foo: "Parrot", bar: mocked.bar });
+    expect(mocked).toEqual({
+      name: mocked.name,
+      pet: "Parrot",
+    });
   });
 
   it("should allow mocking primitive values", () => {
     const buildMock = createMockBuilder((f) => f.animal.cat());
 
     const mocked = buildMock();
-
     expect(mocked).toEqual(mocked);
   });
 
   it("should allow overriding primitive value mocks", () => {
     const buildMock = createMockBuilder<string | null | undefined>((f) => f.animal.cat());
 
-    const mocked1 = buildMock("Parrot");
+    expect(buildMock("Parrot")).toEqual("Parrot");
 
-    expect(mocked1).toEqual("Parrot");
+    expect(buildMock(null)).toEqual(null);
 
-    const mocked2 = buildMock(null);
-
-    expect(mocked2).toEqual(null);
-
-    const mocked3 = buildMock(undefined);
-
-    expect(mocked3).toEqual(undefined);
+    expect(buildMock(undefined)).toEqual(undefined);
   });
 
   it("should allow overriding primitive value mocks with a function", () => {
     const buildMock = createMockBuilder<string | null | undefined>((f) => f.animal.cat());
 
-    const mocked1 = buildMock(() => "Parrot");
+    expect(buildMock(() => "Parrot")).toEqual("Parrot");
 
-    expect(mocked1).toEqual("Parrot");
+    expect(buildMock(() => null)).toEqual(null);
 
-    const mocked2 = buildMock(() => null);
-
-    expect(mocked2).toEqual(null);
-
-    const mocked3 = buildMock(() => undefined);
-
-    expect(mocked3).toEqual(undefined);
+    expect(buildMock(() => undefined)).toEqual(undefined);
   });
 });
